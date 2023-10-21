52039ad455c1d266971b5da89cefd9dbd262acb5
Prince <patelprince3163@gmail.com>
2023-10-21 18:02:11 +0530

    21/10

--- app/src/main/java/com/example/assignment2_21012011099/MainActivity.kt
+++ app/src/main/java/com/example/assignment2_21012011099/MainActivity.kt
@@ -10,10 +10,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var add:Button=findViewById(R.id.add)
-
-        add.setOnClickListener(){
-
-        }
     }
 }

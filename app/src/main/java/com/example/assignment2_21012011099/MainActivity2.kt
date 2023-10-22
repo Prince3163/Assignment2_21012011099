7bdbe08cb9bc9ec936c833cbbad4662f43578ee5
Prince <patelprince3163@gmail.com>
2023-10-22 18:08:10 +0530

    22/10

--- app/src/main/java/com/example/assignment2_21012011099/MainActivity2.kt
+++ app/src/main/java/com/example/assignment2_21012011099/MainActivity2.kt
@@ -2,10 +2,17 @@ package com.example.assignment2_21012011099
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var back:ImageButton=findViewById(R.id.back)
+
+        back.setOnClickListener(){
+            finish()
+        }
     }
 }

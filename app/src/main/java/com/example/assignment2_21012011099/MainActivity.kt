733dd4269b1424e679b5107e4091c1000dd3e217
Prince <patelprince3163@gmail.com>
2023-10-22 16:14:55 +0530

    22/10

--- app/src/main/java/com/example/assignment2_21012011099/MainActivity.kt
+++ app/src/main/java/com/example/assignment2_21012011099/MainActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        var pb : ImageButton=findViewById(R.id.ib1)
+
+        pb.setOnClickListener(){
+            Intent(this,MainActivity2 :: class.java).also { startActivity(it) }
+        }
     }
 }
